5fe0d9518f0da38ba6b8422b949d72313fca3e49
rladudg <165765314+rladudg@users.noreply.github.com>
2026-06-18 11:02:16 +0900

    Delete 프라이버시 필터 프로그램(PFP)/mosaicproject directory

--- 프라이버시 필터 프로그램(PFP)/mosaicproject/FaceDetection.h
+++
@@ -1,6 +0,0 @@
-#ifndef FACE_DETECTION_H
-#define FACE_DETECTION_H
-
-void selectMode();
-
-#endif // FACE_DETECTION_H
--- 프라이버시 필터 프로그램(PFP)/mosaicproject/mosaic.h
+++
@@ -1,8 +0,0 @@
-#ifndef MOSAIC_H
-#define MOSAIC_H
-
-#include <opencv2/opencv.hpp>
-
-void mosaic(cv::Mat& img, cv::Rect face, double scale = 0.05);
-
-#endif // MOSAIC_H
